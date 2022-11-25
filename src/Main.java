88208bb3c1d94e77dc4998ed75d7a5f6e4215b91
User <dg21stew@gmail.com>
2022-11-25 15:38:39 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    // convert minutes and seconds into XXh:YYm:ZZs format
+
+    public static void main(String[] args) {
+
+        SecondsToMinutes secondsToMinutes = new SecondsToMinutes();
+        secondsToMinutes.getDurationString(23, 6534);
+
+
+    }
+
+
+}
